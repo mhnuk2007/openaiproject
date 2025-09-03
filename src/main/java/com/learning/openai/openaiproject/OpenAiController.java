1f0f72d0195c529c0c6de6d4cdaf817e339fd164
Mohan Lal <mhnuk2007@gmail.com>
2025-09-03 20:30:22 +0500

    feat: Modify OpenAiController add Embbeding Model

--- src/main/java/com/learning/openai/openaiproject/OpenAiController.java
+++ src/main/java/com/learning/openai/openaiproject/OpenAiController.java
@@ -7,7 +7,10 @@ import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +20,10 @@ import java.util.Map;
 public class OpenAiController {
 
     private ChatClient chatClient;
+
+    @Autowired
+    @Qualifier("openAiEmbeddingModel")
+    private EmbeddingModel embeddingModel;
 
     public OpenAiController(OpenAiChatModel chatModel){
         this.chatClient=ChatClient.create(chatModel);
@@ -71,9 +78,12 @@ public class OpenAiController {
                 .call()
                 .content();
         return response;
-
-
     }
 
+    @PostMapping("/api/embedding")
+    public float[] embedding(@RequestParam String text){
+        return embeddingModel.embed(text);
+
+    }
 
 }
